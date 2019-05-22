63de14138e2bfbc8df6fe6ec10430c1c6b91b7c3
WebProduction <WebProduction@admins-iMac-10.local>
2019-05-22 15:46:26 -0400

    added back index query

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -4,18 +4,31 @@
     <!-- Learn how to use images here: https://gridsome.org/docs/images -->
     <g-image alt="Example image" src="~/favicon.png" width="135" />   
 
-
- <p>Dang</p>
+  <h1>{{$page.contentfulPage.title}}</h1>
+  <p v-html="richtextToHTML($page.contentfulPage.mainContent)"></p>
   </Layout>
 </template>
 
-
+<page-query>
+query Page {
+	contentfulPage(id:"695ls1eS7WSVhC1ZhV5gaz") {
+    id
+    title
+    mainContent
+  }
+}
+</page-query>
 
 <script>
-
+import { documentToHtmlString } from '@contentful/rich-text-html-renderer'
 export default {
   metaInfo: {
     title: 'Gridsome Contentful'
+  },
+    methods: {
+    richtextToHTML (content) {
+      return documentToHtmlString(content)
+    }
   }
 }
 </script>
